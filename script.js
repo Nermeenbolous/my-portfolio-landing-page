9492f4a6dc258d9fc59b6c5bd4c4425be090edc0
Nermeenbolous <nbolous@lakelandcollege.edu>
2026-02-23 20:44:21 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const toggleBtn = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme');
+
+// Check for saved theme on load
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+}
+
+toggleBtn.addEventListener('click', () => {
+    let theme = document.documentElement.getAttribute('data-theme');
+    
+    if (theme === 'dark') {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+});
